a36e498da051a89ab5bd69202995b8cb52c83baf
Sheikhman_Kalderon <duah229@outlook.com>
2025-06-26 12:05:44 +0000

    fix: remove mapping target annotation placed on studentresponsedto

--- student-service/src/main/java/com/school/student_service/mapper/StudentMapper.java
+++ student-service/src/main/java/com/school/student_service/mapper/StudentMapper.java
@@ -1,6 +1,7 @@
 package com.school.student_service.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import com.school.student_service.dto.StudentRequestDto;
 import com.school.student_service.dto.StudentResponseDto;
@@ -11,12 +12,15 @@ import com.school.student_service.entity.Student;
  * dependencies are two, path, and dependency api
  * have to annotae this for spring to identify this as a component, spring
  * becomes responsible for its instantiation, management as a spring bean
+ * 
  */
 
 @Mapper(componentModel = "spring")
 public interface StudentMapper {
     
+    @Mapping(target = "id", ignore = true)
     Student toEntity(StudentRequestDto dto);
 
+    
     StudentResponseDto toStudentResponseDto(Student student);
 }
