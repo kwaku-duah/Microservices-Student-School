35ec6a88c020e0b6a572f396e13555ad8a3e388b
Sheikhman_Kalderon <duah229@outlook.com>
2025-06-26 15:35:26 +0000

    feature: add mapper request and response to the project

---
+++ school-core/src/main/java/com/school/school_core/mapper/SchoolMapper.java
@@ -0,0 +1,17 @@
+package com.school.school_core.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import com.school.school_core.dto.SchoolRequestDto;
+import com.school.school_core.dto.SchoolResponseDto;
+import com.school.school_core.entity.School;
+
+@Mapper(componentModel = "spring")
+public interface SchoolMapper {
+    
+    @Mapping(target = "schoolId", ignore = true)
+    School toEntity(SchoolRequestDto dto);
+
+    SchoolResponseDto toResponseDto(School school);
+}
