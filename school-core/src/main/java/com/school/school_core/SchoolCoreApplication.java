ac5e14f471fe38bdab19972440c87b14359f6594
Sheikhman_Kalderon <duah229@outlook.com>
2025-06-26 15:13:09 +0000

    chore: add school service project structure

---
+++ school-core/src/main/java/com/school/school_core/SchoolCoreApplication.java
@@ -0,0 +1,13 @@
+package com.school.school_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolCoreApplication.class, args);
+	}
+
+}
