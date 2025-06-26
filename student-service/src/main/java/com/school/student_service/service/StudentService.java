5f4f472d97ad308b35acc3c0caa6c78a603994ed
Sheikhman_Kalderon <duah229@outlook.com>
2025-06-26 12:19:48 +0000

    feature: add service interface for implementation

---
+++ student-service/src/main/java/com/school/student_service/service/StudentService.java
@@ -0,0 +1,18 @@
+package com.school.student_service.service;
+
+import java.util.List;
+
+import com.school.student_service.dto.StudentRequestDto;
+import com.school.student_service.dto.StudentResponseDto;
+
+/*
+ * discusses what should be done
+ * an interface is a blueprints that details what methods a class should have
+ * not how it should implement them
+ */
+
+public interface StudentService {
+    StudentResponseDto createStudent(StudentRequestDto dto);
+    List<StudentResponseDto> findAllStudents();
+    StudentResponseDto getStudentById(Long id);
+}
