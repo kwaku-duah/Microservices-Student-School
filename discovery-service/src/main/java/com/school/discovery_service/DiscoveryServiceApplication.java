196efe5a970bbe68ba1b6363693b991e5cf5221f
Sheikhman_Kalderon <duah229@outlook.com>
2025-06-28 17:29:31 +0000

    feature: add configuration for discovery service

---
+++ discovery-service/src/main/java/com/school/discovery_service/DiscoveryServiceApplication.java
@@ -0,0 +1,19 @@
+package com.school.discovery_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+/*
+ * adding enable eureka server based registry
+ * annotation to make
+ */
+
+@EnableEurekaServer
+@SpringBootApplication
+public class DiscoveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryServiceApplication.class, args);
+	}
+
+}
